0bca9c59aa21e814f9ea01500086b720fb411790
willy Cedeno <willycedeno@kimerasoft-ec.com>
2018-10-20 12:24:58 -0500

    primer commit

---
+++ app/Http/Controllers/FireReport/FireReportApiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\FireReport;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FireReportApiController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Location/UserLocationApiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Location;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserLocationApiController extends Controller
+{
+    //
+}
